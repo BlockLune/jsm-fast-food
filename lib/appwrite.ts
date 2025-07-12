f07a9b14bd024adc96270f2e1cd820f4a0a1222f
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-07-12 09:41:15 +0800

    chore: add database collections on appwrite

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -9,9 +9,14 @@ import {
 } from "react-native-appwrite";
 
 export const appwriteConfig = {
+  bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID!,
+  categoriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_CATEGORIES_COLLECTION_ID!,
+  customizationsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_CUSTOMIZATIONS_COLLECTION_ID!,
   databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!,
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
-  platform: "cc.blocklune.jsmfastfood",
+  menuCollectionId: process.env.EXPO_PUBLIC_APPWRITE_MENU_COLLECTION_ID!,
+  menuCustomizationsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_MENU_CUSTOMIZATIONS_COLLECTION_ID!,
+  platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
   userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID!,
 };
