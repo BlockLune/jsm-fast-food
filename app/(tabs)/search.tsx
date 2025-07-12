9748756501bac1b34ccb7cdac21344307b81b2ea
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-07-12 13:38:47 +0800

    chore: seed the db

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,20 @@
-import { Text, View } from "react-native";
+import seed from "@/constants/seed";
+import { Button, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Search = () => {
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Search</Text>
+        <Button
+          title="Seed"
+          onPress={() => {
+            seed().catch(console.error);
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
